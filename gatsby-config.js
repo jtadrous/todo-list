726157410e8b6c0da8c14fd0dcd6bfce57863dd2
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2021-09-29 13:47:25 -0400

    Updated live link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
     //fix this to add Web4 link
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl:
+      "https://in-info-web4.informatics.iupui.edu/~jtadrous/N322/todo-list",
     title: "todo-list",
   },
   plugins: [],
   //fix this to be yours
-  //pathPrefix: '/~tstreets/todo-list'
+  pathPrefix: "/~jtadrous/N322/todo-list",
 };
